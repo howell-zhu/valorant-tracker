60221e79c93e5128aa44c7d3fd2288a8411d2c4c
Howell Zhu <howell.chu@icloud.com>
2026-05-02 01:28:46 -0400

    Use SQLite database on Railway

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='sqlite:///db.sqlite3')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
