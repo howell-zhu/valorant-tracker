2dcdb5a07ecd1ee91004b8f24bddf6016d181891
Howell Zhu <howell.chu@icloud.com>
2026-05-01 23:42:31 -0400

    update

--- project/settings.py
+++ project/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db(default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    'default': env.db('DATABASE_URL', default='sqlite:///db.sqlite3')
 }
 
 
